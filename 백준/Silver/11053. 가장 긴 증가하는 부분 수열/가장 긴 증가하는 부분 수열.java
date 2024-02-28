21fa22a140587f3056a5e5309dd08d405d015cfa
dev_kong <96301958+green-kong@users.noreply.github.com>
2024-02-28 11:29:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 14884 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    // https://www.acmicpc.net/problem/11053
+    static int[] arr;
+    static int[] dp;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        dp = new int[n];
+
+        final StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            recursive(i);
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+
+    private static int recursive(final int curIdx) {
+        if (dp[curIdx] != 0) {
+            return dp[curIdx];
+        }
+        int max = 1;
+
+        for (int i = curIdx - 1; i >= 0; i--) {
+            if (arr[i] < arr[curIdx]) {
+                max = Math.max(max, recursive(i) + 1);
+            }
+        }
+        dp[curIdx] = max;
+        return max;
+    }
+}
